cfefa76c35002e7eeb8f9de3accc4f68ddc71703
Siddique Muhammad <siddique2277860@gmail.com>
2023-05-09 11:09:42 +0200

    frontend: resize searching, selected result

--- frontend/next-project/src/pages/search/index.tsx
+++ frontend/next-project/src/pages/search/index.tsx
@@ -76,94 +76,95 @@ const SearchPage = () => {
       {/* General Div*/}
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 m-3">
 
-        {/* Heading */}
-        <div className="text-center p-5">
-          <h1 className="text-4xl font-bold mb-4">
-            Semantic Search Engine
-          </h1>
-        {/* End Heading */}
-        </div>
+      {/* Heading */}
+      <div className="text-center p-5">
+        <h1 className="text-4xl font-bold mb-4">
+          Semantic Search Engine
+        </h1>
+      {/* End Heading */}
+      </div>
 
-        {/* Search (Results and selected) */}
-        <div className="">
-          
-          {/* Searching (Search and results) */}
-          <div className="">
-
-            <form onSubmit={handleSubmit}>
-
-              {/* Search input */}
-              <div className="flex items-center mb-4">
-                <label htmlFor="search" className="mr-4 font-medium text-xl">
-                  Search:
-                </label>
-                <input
-                  type="text"
-                  value={query} onChange={(e) => setQuery(e.target.value)}
-                  id="search"
-                  name="search"
-                  className="border border-gray-300 px-4 py-2 rounded-md flex-grow"
-                />
-                <br></br>
-
-                {/* Search button */}
-                <div>
-                  <button
-                  type="submit"
-                    id="search-document-btn"
-                    onClick={() => console.log("Search documents")}
-                    className="ml-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-                  >
-                    Search documents  
-                  </button>
-
-                {/* End Search button */}
-                </div>
-
-              {/* End Search Input */}
-              </div>
-
-            </form>
-
-            {/* Show message, count results found  */}
-            <div>
-              {message && <p className="text-sm">{message}</p>}
-            {/* End show message */}
-            </div>
-
-
-            {/* Results */}
-            <div className="grid grid-cols-1 gap-5 mt-8">
-              {results.map((result) => (
-                <div key={result} className={`bg-gray-200 p-4 rounded-lg ${selectedResult === result ? 'border-2 border-blue-500' : ''}`}>
-                <label>
-                  <input type="checkbox" checked={selectedResult === result} onChange={() => setSelectedResult(result)} />
-                  <span className="ml-2">{result}</span>
-                </label>
-              </div>
-              ))}
-            {/* End Results */}
-            </div>
+      {/* Search (Results and selected) */}
+      <div className="flex flex-row">
         
-          {/* End Searching (Search and results) */}
-          </div>
+        {/* Searching (Search and results) */}
+        <div className="basis-full">
 
-          {/* Selected Result */}
-          <div>
-            {selectedResult !== null && (
-              <div className="mt-4 p-4 bg-gray-200 rounded-lg">
-                <h2 className="text-lg font-bold mb-2">{selectedResult}</h2>
-                <p>This is the data for {selectedResult}.</p>
+          <form onSubmit={handleSubmit}>
+
+            {/* Search input */}
+            <div className="flex items-center mb-4">
+              <label htmlFor="search" className="mr-4 font-medium text-xl">
+                Search:
+              </label>
+              <input
+                type="text"
+                value={query} onChange={(e) => setQuery(e.target.value)}
+                id="search"
+                name="search"
+                className="border border-gray-300 px-4 py-2 rounded-md flex-grow"
+              />
+              <br></br>
+
+              {/* Search button */}
+              <div>
+                <button
+                type="submit"
+                  id="search-document-btn"
+                  onClick={() => console.log("Search documents")}
+                  className="ml-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                >
+                  Search documents  
+                </button>
+
+              {/* End Search button */}
               </div>
-            )}
-          {/* End Selected Result */}
+
+            {/* End Search Input */}
+            </div>
+
+          </form>
+
+          {/* Show message, count results found  */}
+          <div>
+            {message && <p className="text-sm">{message}</p>}
+          {/* End show message */}
           </div>
 
-        {/* End Search (Results, search, and selected)*/}
+
+          {/* Results */}
+          <div className="grid grid-cols-1 gap-5 mt-8">
+            {results.map((result) => (
+              <div key={result} className={`bg-gray-200 p-4 rounded-lg ${selectedResult === result ? 'border-2 border-blue-500' : ''}`}>
+              <label>
+                <input type="checkbox" checked={selectedResult === result} onChange={() => setSelectedResult(result)} />
+                <span className="ml-2">{result}</span>
+              </label>
+            </div>
+            ))}
+          {/* End Results */}
+          </div>
+
+        {/* End Searching (Search and results) */}
         </div>
+
+        {/* Selected Result */}
+        <div className="basis-1/2">
+          {selectedResult !== null && (
+            <div className="ml-9 p-4 bg-gray-200 rounded-lg">
+              <h2 className="text-lg font-bold mb-2">{selectedResult}</h2>
+              <p>This is the data for {selectedResult}.</p>
+            </div>
+          )}
+        {/* End Selected Result */}
+        </div>
+
+      {/* End Search (Results, search, and selected)*/}
+      </div>
 
       {/* End General Div*/}
       </div>
+
 
 
       {/* Footer */}
