e824682be2f2a6cbdac48681bdf550bb52423c86
Siddique Muhammad <siddique2277860@gmail.com>
2023-05-09 11:17:11 +0200

    frontend:search page for elasticsearch

---
+++ frontend/next-project/src/pages/searchdoc/index.tsx
@@ -0,0 +1,39 @@
+/**
+   * @file index.tsx Typescript file. 
+   * @description Search section
+   * @version 1.0
+   * @author Siddique Muhammad
+*/
+
+/**
+ * @imports
+ * NavBar component
+ * Footer 
+ * React library, useState
+ */
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
+import React, { useState } from "react";
+
+
+/**
+ * @constant SearchPage
+ * @returns html page
+ */
+const SearchDocs = () => { 
+
+    return (
+        <div>
+            <NavBar></NavBar>
+
+            <div>
+                <h1></h1>
+            </div>
+
+
+        </div>
+    );
+}
+
+
+export default SearchDocs;
