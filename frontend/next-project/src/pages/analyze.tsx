bb6f0535f580f1059310e4604ba386a7a3bcb3c3
Siddique ms <siddique2277860@gmail.com>
2023-05-23 11:07:58 +0000

    analyze page updated

--- frontend/next-project/src/pages/analyze.tsx
+++ frontend/next-project/src/pages/analyze.tsx
@@ -1,7 +1,7 @@
 /**
- * @file corpus main page
+ * @file analyze main page
  * @author Siddique Muhammad
- * @version 1.1
+ * @version 1.2
  */
 
 /**
@@ -16,62 +16,65 @@ import Layout from '@/components/Layout';
 import { Card, CardContent, Typography } from '@mui/material';
 
 
-
 /**
- * @const CorpusPage
- * @description Manages the analization section
+ * @const AnalyzePage
+ * @description Manages the analization section. 
+ *              Lists corpus and selecting one, redirects to dashboard.
  * @returns html page
  */
-const CorpusPage = () => (
-  <Layout>
-    {/* Main Div */}
-    <div className="flex flex-col bg-gray-100 ">
+const AnalyzePage = () => {
 
-      {/* Content */}
-      <div className="flex-grow ">
+  return (
+    <Layout>
+      {/* Main Div */}
+      <div className="flex flex-col bg-gray-100 ">
 
-        <div className="flex flex-col items-center justify-center my-8">
-          <div className="text-center w-10/12">
-            <h1 className="text-4xl font-bold mb-6">Corpus Page</h1>
+        {/* Content */}
+        <div className="flex-grow ">
 
-            {/* Text */}
-            <div className='bg-white rounded-lg shadow-lg p-8 text-justify'>
-              <p className="text-lg text-gray-700 mb-4">The corpus page is a section of the application that focuses on managing text corpora. A corpus is a collection of texts, documents, or conversations used for linguistic or other types of analysis. In the context of the application, corpora are collections of texts related to a specific topic, such as biomedicine or linguistics.</p>
-              <p className="text-lg text-gray-700 mb-4">The corpus page allows users to create, edit, and delete text corpora. They can also add individual documents to the corpus or import sets of documents in zip format. Additionally, users can view and filter the list of available corpora based on various criteria, such as name, language, or number of documents.</p>
-              <p className="text-lg text-gray-700 mb-4">With the corpus page, researchers and analysts can easily organize and curate large-scale text data and use it for various purposes, such as language modeling, information retrieval, or natural language processing.</p>
+          <div className="flex flex-col items-center justify-center my-8">
+            <div className="text-center w-10/12">
+              <h1 className="text-4xl font-bold mb-6">Corpus Page</h1>
 
-            {/* End Text */}
-            </div>
-            
-            {/* Buttons */}
-            <div className="flex justify-center space-x-4 mt-8">
+              {/* Text */}
+              <div className='bg-white rounded-lg shadow-lg p-8 text-justify'>
+                <p className="text-lg text-gray-700 mb-4">The corpus page is a section of the application that focuses on managing text corpora. A corpus is a collection of texts, documents, or conversations used for linguistic or other types of analysis. In the context of the application, corpora are collections of texts related to a specific topic, such as biomedicine or linguistics.</p>
+                <p className="text-lg text-gray-700 mb-4">The corpus page allows users to create, edit, and delete text corpora. They can also add individual documents to the corpus or import sets of documents in zip format. Additionally, users can view and filter the list of available corpora based on various criteria, such as name, language, or number of documents.</p>
+                <p className="text-lg text-gray-700 mb-4">With the corpus page, researchers and analysts can easily organize and curate large-scale text data and use it for various purposes, such as language modeling, information retrieval, or natural language processing.</p>
 
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-all duration-500 transform hover:scale-110">
-                <Link href='/corpus/insert'>
-                  Insert a Corpus
-                </Link>
-              </button>
+              {/* End Text */}
+              </div>
               
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-all duration-500 transform hover:scale-110">
-                <Link href='/corpus/list'>
-                  List Corpus
-                </Link>
-              </button>
+              {/* Buttons */}
+              <div className="flex justify-center space-x-4 mt-8">
 
-            {/* End Buttons */}
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-all duration-500 transform hover:scale-110">
+                  <Link href='/corpus/insert'>
+                    Insert a Corpus
+                  </Link>
+                </button>
+                
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-all duration-500 transform hover:scale-110">
+                  <Link href='/corpus/list'>
+                    List Corpus
+                  </Link>
+                </button>
+
+              {/* End Buttons */}
+              </div>
+
             </div>
 
           </div>
 
+        {/* End Content */}
         </div>
 
-      {/* End Content */}
+
+      {/* End Main Div */} 
       </div>
+    </Layout>
+  );
+}
 
-
-    {/* End Main Div */} 
-    </div>
-  </Layout>
-);
-
-export default CorpusPage;
+export default AnalyzePage;
