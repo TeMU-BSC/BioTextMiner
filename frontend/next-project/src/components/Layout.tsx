c470de347d57785d2eb7488bca5e82f392e08054
Siddique ms <siddique2277860@gmail.com>
2023-05-23 09:40:02 +0000

    Layout component updated

--- frontend/next-project/src/components/Layout.tsx
+++ frontend/next-project/src/components/Layout.tsx
@@ -1,22 +1,56 @@
-import { ReactNode } from 'react';
-import Footer from './footer';
-import NavBar from './navbar';
+/**
+ * @file Layout.tsx
+ * @description Layout for the pages
+ * @author Siddique Muhammad
+ * @version 1.1.
+ */
 
+
+/**
+ * @imports
+ * @ReactNode from react library
+ * @Footer component
+ * @NavBar component
+ */
+import { ReactNode } from 'react';
+import Footer from './Footer';
+import NavBar from './Navbar';
+
+/**
+ * @interface LayoutProps
+ */
 interface LayoutProps {
   children: ReactNode;
 }
 
 
+/**
+ * @const Layout
+ * @return html layout
+ */
 const Layout = ({ children }: LayoutProps) => {
+
   return (
-    <div className="bg-gray-100 min-h-screen">
+
+
+    // General Div
+    <div className="flex flex-col min-h-screen">
+
+      {/* Include NavBar */}
       <NavBar/>
-      {children}
+
+      {/* Children data */}
+      <div className="flex-grow">{children}</div>
+
+      {/* Include Footer */}
       <div className='bottom-0 w-full text-center'>
         <Footer/>
       </div>
+
     </div>
   );
 };
 
+
+// Export Layout
 export default Layout;
