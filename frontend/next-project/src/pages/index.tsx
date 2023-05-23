55d91a8405c816cabee76e1d7924ad081f9a306c
Siddique ms <siddique2277860@gmail.com>
2023-05-23 11:08:55 +0000

    bg parallax home page done

--- frontend/next-project/src/pages/index.tsx
+++ frontend/next-project/src/pages/index.tsx
@@ -20,41 +20,49 @@ import Layout from '@/components/Layout';
  */
 const HomePage = () => (
   
-  <Layout>
+    <Layout>
 
-    <div className="flex-grow">
+    <header
+      className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img"
+      >
+      <div className="p-5 text-2xl bg-purple-300 bg-opacity-50 rounded-xl">
+          Welcome to BioTextMiner!
+      </div>
+    </header>
+    
+    <main className="flex-grow">
 
       {/* Section welcome */}
       <section>
-        <div className=" justify-center items-center w-8/12 mx-auto my-8">
+      <div className=" justify-center items-center w-8/12 mx-auto my-8">
           <div className="text-center">
-            <h1 className="text-4xl font-bold mb-6">Welcome to BioTextMiner</h1>
-            
-            <div className=''>
+          <h1 className="text-4xl font-bold mb-6">Welcome to BioTextMiner</h1>
+          
+          <div className=''>
               <p className="text-l animate-pulse text-black-900 text-xl text-justify">BioTextMiner is a web application developed by the NLP4BIA that provides a user-friendly interface for corpus control of biomedical corpora. With BioTextMiner, researchers can easily manage and manipulate large-scale biomedical text data by organizing and curating it in a centralized database </p>
-            </div>
           </div>
-        </div>
+          </div>
+      </div>
       </section>
 
       {/* Section image */}
       <section>
-        <div className="max-w-screen-lg mx-auto">
+      <div className="max-w-screen-lg mx-auto">
           <div className="bg-white rounded-md overflow-hidden shadow-md">
-            <Image
+          <Image
               className="object-cover"
               src="/../public/nlp.jpg"
               alt="Picture of the author"
-              width={950}
+              width={800}
               height={100}
-            />
+          />
           </div>
-        </div>
+      </div>
       </section>
 
-    </div>
-
-  </Layout>
+    </main>
+  
+    </Layout>
 )
 
 export default HomePage
